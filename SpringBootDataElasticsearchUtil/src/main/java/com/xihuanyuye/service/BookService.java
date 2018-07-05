c7ea8c7d71b50930c2fed1c2afc5dd5a40ef02ee
xihuanyuyetsinghua <xihuanyuye@126.com>
2018-07-05 16:24:05 +0800

    springbootDataElasticsearch

---
+++ SpringBootDataElasticsearchUtil/src/main/java/com/xihuanyuye/service/BookService.java
@@ -0,0 +1,29 @@
+package com.xihuanyuye.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import com.xihuanyuye.model.Book;
+
+/**
+ * 描述:
+ *
+ * @author yanpenglei
+ * @create 2017-11-02 14:48
+ **/
+public interface BookService {
+
+    Book save(Book book);
+
+    void delete(Book book);
+
+    Book findOne(String id);
+
+    Iterable<Book> findAll();
+
+    Page<Book> findByAuthor(String author, PageRequest pageRequest);
+
+    List<Book> findByTitle(String title);
+}
---
+++ SpringBootDataElasticsearchUtil/src/main/java/com/xihuanyuye/service/BookServiceImpl.java
@@ -0,0 +1,56 @@
+package com.xihuanyuye.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import com.xihuanyuye.model.Book;
+import com.xihuanyuye.repository.BookRepository;
+
+/**
+ * 描述:
+ *
+ * @author yanpenglei
+ * @create 2017-11-02 14:49
+ **/
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+
+    @Override
+    public Book save(Book book) {
+        return bookRepository.save(book);
+    }
+
+    @Override
+    public void delete(Book book) {
+        bookRepository.delete(book);
+    }
+
+    @Override
+    public Book findOne(String id) {
+        return bookRepository.findOne(id);
+    }
+
+    @Override
+    public Iterable<Book> findAll() {
+        return bookRepository.findAll();
+    }
+
+    @Override
+    public Page<Book> findByAuthor(String author, PageRequest pageRequest) {
+        return bookRepository.findByAuthor(author, pageRequest);
+    }
+
+    @Override
+    public List<Book> findByTitle(String title) {
+        return bookRepository.findByTitle(title);
+    }
+
+}
