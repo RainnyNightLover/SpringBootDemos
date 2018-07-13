92807d6d98d54e5b416a9ca3e7fa25a4cc3bc2aa
xihuanyuyetsinghua <xihuanyuye@126.com>
2018-07-13 15:57:56 +0800

    Springbootdatamongodb

---
+++ SpringBootDataMongodb/src/main/java/com/xihuanyuye/DemoDao.java
@@ -0,0 +1,13 @@
+package com.xihuanyuye;
+
+
+public interface DemoDao {
+
+    void saveDemo(DemoEntity demoEntity);
+
+    void removeDemo(Long id);
+
+    void updateDemo(DemoEntity demoEntity);
+
+    DemoEntity findDemoById(Long id);
+}
