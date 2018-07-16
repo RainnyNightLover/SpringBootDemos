1ccc1dc7b35b02e1de9c280c230c8bd9026f7a49
xihuanyuyetsinghua <xihuanyuye@126.com>
2018-07-16 11:13:09 +0800

    springboot-logback

---
+++ SpringBootLogback/src/main/java/com/xihuanyuye/controller/IndexController.java
@@ -0,0 +1,40 @@
+package com.xihuanyuye.controller;
+
+import java.time.LocalDateTime;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xihuanyuye.config.annotation.Log;
+
+
+@RestController
+@RequestMapping(value = "/index")
+public class IndexController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
+    /**
+     * http://127.0.0.1:8080/index/?content="我是测试内容"
+     *
+     * @param content
+     * @return
+     */
+    @Log("首页IndexController")
+    @RequestMapping(value="", method= RequestMethod.GET)
+    public String index(@RequestParam String content) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        LOGGER.trace("请求参数：content:{}", content);
+        LOGGER.debug("请求参数：content:{}", content);
+        LOGGER.info("请求参数：content:{}", content);
+        LOGGER.warn("请求参数：content:{}", content);
+        LOGGER.error("请求参数：content:{}", content);
+
+        return localDateTime + ",content:" + content;
+    }
+}
